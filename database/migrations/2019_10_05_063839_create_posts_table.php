72c1790628940cb55470ff6cc9e2caa2535186b9
Salman778 <alk-333@hotmail.com>
2019-10-05 10:04:29 +0300

    create migrate file to ceae posts and then by run this command #php artisan make:migration create_posts_table, and then add our filds into it

---
+++ database/migrations/2019_10_05_063839_create_posts_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('title');
+            $table->string('image_url')->nullable();
+            $table->text('body');
+            $table->timestamps();
+            $table->bigInteger('user_id')->unsigned();
+
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('posts', function (Blueprint $table) {
+            $table->dropForeign('posts_user_id_foreign');
+            $table->dropColumn('user_id');
+        });
+        Schema::dropIfExists('posts');
+    }
+}
